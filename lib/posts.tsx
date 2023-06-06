8b6df9c1bfe603703e5d7e2efd17af415fdcbf06
Aminul Kibria <aminulkibria@gmail.com>
2023-06-06 22:15:12 +0600

    git commit

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -17,7 +17,7 @@ export async function getPostByName(
   fileName: string
 ): Promise<BlogPost | undefined> {
   const res = await fetch(
-    `https://raw.githubusercontent.com/dksifatofficial/new-blogposts/main/${fileName}`,
+    `https://raw.githubusercontent.com/dksifatofficial/new-blogposts/master/${fileName}`,
     {
       headers: {
         Accept: "application/vnd.github+json",
@@ -25,13 +25,13 @@ export async function getPostByName(
         "X-GitHub-Api-Version": "2022-11-28",
       },
     }
-  );
+  )
 
   if (!res) return undefined;
 
   const rawMDX = await res.text();
 
-  if (rawMDX === "404: Not Found") return undefined;
+  if (rawMDX === "404: Not Found") return undefined
 
   const { frontmatter, content } = await compileMDX<{
     title: string;
@@ -60,7 +60,7 @@ export async function getPostByName(
     },
   });
 
-  const id = fileName.replace(/\.mdx$/, "");
+  const id = fileName.replace(/\.mdx$/, "")
 
   const blogPostObj: BlogPost = {
     meta: {
@@ -77,7 +77,7 @@ export async function getPostByName(
 
 export async function getPostsMeta(): Promise<Meta[] | undefined> {
   const res = await fetch(
-    "https://api.github.com/repos/dksifatofficial/new-blogposts/git/trees/main?recursive=1",
+    "https://api.github.com/repos/dksifatofficial/new-blogposts/git/trees/master?recursive=1",
     {
       headers: {
         Accept: "application/vnd.github+json",
@@ -87,23 +87,21 @@ export async function getPostsMeta(): Promise<Meta[] | undefined> {
     }
   );
 
-  if (!res) return undefined;
+  if (!res) return undefined
 
-  const repoFiletree: Filetree = await res.json();
+  const repoFiletree: Filetree = await res.json()
 
-  const filesArray = repoFiletree.tree
-    .map((obj) => obj.path)
-    .filter((path) => path.endsWith(".mdx"));
+  const filesArray = repoFiletree.tree.map(obj => obj.path).filter(path => path.endsWith('.mdx'))
 
   const posts: Meta[] = [];
 
   for (const file of filesArray) {
-    const post = await getPostByName(file);
+    const post = await getPostByName(file)
     if (post) {
       const { meta } = post;
       posts.push(meta);
     }
   }
 
-  return posts.sort((a, b) => (a.date < b.date ? 1 : -1));
+  return posts.sort((a, b) => (a.date < b.date ? 1 : -1))
 }
